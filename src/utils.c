79eb056b942267d40ed01b4b0e0dd823663777ec
bkaras-g <bkaras-g@student.42.fr>
2025-12-28 15:58:43 +0100

    norminette utils.c

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: bkaras-g <bkaras-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/16 17:05:16 by bkaras-g          #+#    #+#             */
-/*   Updated: 2025/12/28 14:25:26 by bkaras-g         ###   ########.fr       */
+/*   Updated: 2025/12/28 15:58:24 by bkaras-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,34 +39,41 @@ int	ft_atoi(const char *nptr)
 	return (res * sign);
 }
 
-long ft_get_elapsed_time_microseconds(struct timeval start, struct timeval end)
+long	ft_get_elapsed_time_microseconds(struct timeval start,
+		struct timeval end)
 {
-    return (end.tv_sec - start.tv_sec) * 1000000L + (end.tv_usec - start.tv_usec);
+	return ((end.tv_sec - start.tv_sec) * 1000000L
+		+ (end.tv_usec - start.tv_usec));
 }
 
 /*
-* usleep() guarantees a minimum time of sleep but generally makes the thread sleep
-* more than asked (approx. 70 ms). To avoid this, this function usleeps in chunks
-* and computes the actual elapsed time. When rem <= 1000, the thread busy-waits: it
-* loops computing elapsed again and again until it's > usec
-*
-* @param usec the time to wait in microseconds
-*/
-void ft_precise_usleep(long usec) {
-    struct timeval start, current;
-    long elapsed;
-    long rem;
 
-    gettimeofday(&start, NULL);
-    do {
-        gettimeofday(&current, NULL);
-        elapsed = ft_get_elapsed_time_microseconds(start, current);
-        rem = usec - elapsed;
+	* usleep() guarantees a minimum time of sleep but generally makes
+	the thread sleep more than asked (approx. 70 ms). To avoid this,
+	this function usleeps in chunks
+ * and computes the actual elapsed time. When rem <= 1000,
+	the thread busy-waits: it
+ * loops computing elapsed again and again until it's > usec
+ *
+ * @param usec the time to wait in microseconds
+ */
+void	ft_precise_usleep(long usec)
+{
+	long			elapsed;
+	long			rem;
+	struct timeval	start;
+	struct timeval	current;
 
-        if (rem > 1000)
-            usleep(rem / 2);
-
-    } while (elapsed < usec);
+	gettimeofday(&start, NULL);
+	elapsed = 0;
+	while (elapsed < usec)
+	{
+		gettimeofday(&current, NULL);
+		elapsed = ft_get_elapsed_time_microseconds(start, current);
+		rem = usec - elapsed;
+		if (rem > 1000)
+			usleep(rem / 2);
+	}
 }
 
 long long	ft_get_time(void)
@@ -78,7 +85,8 @@ long long	ft_get_time(void)
 	return ((tv.tv_sec * 1000) + (tv.tv_usec / 1000));
 }
 
-// void	ft_wait_and_check(t_data *data, int time_to_wait)
-// {
-
-// }
+// waits 10 ms then checks if death_flag is set to 1,
+// until time_to_wait is reached
+void	ft_wait_and_check(t_data *data, int time_to_wait)
+{
+}
