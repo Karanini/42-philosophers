0f445977f6a8f8c541499f00246341cb62ad4a39
bkaras-g <bkaras-g@student.42.fr>
2025-12-29 11:50:28 +0100

    delete useless lines in ft_wise_life

--- src/wisdom_lovers_life.c
+++ src/wisdom_lovers_life.c
@@ -6,7 +6,7 @@
 /*   By: bkaras-g <bkaras-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/17 11:30:53 by michel_32         #+#    #+#             */
-/*   Updated: 2025/12/29 11:46:37 by bkaras-g         ###   ########.fr       */
+/*   Updated: 2025/12/29 11:50:02 by bkaras-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,9 @@ void	*ft_wise_life(void *philo_struct)
 {
 	t_data	*data;
 	t_philo	*philo;
-	int		philo_id;
 
 	philo = (t_philo *)philo_struct;
 	data = philo->data;
-	philo_id = philo->philo_id;
 	pthread_mutex_lock(&data->starting_mtx);
 	pthread_mutex_unlock(&data->starting_mtx);
 	philo->start_time = data->philo_tab[0].start_time;
@@ -42,7 +40,6 @@ void	*ft_wise_life(void *philo_struct)
 			break ;
 		if (ft_think(philo) == -1)
 			break ;
-		// ft_print_msg(philo, THINK);
 	}
 	return (NULL);
 }
